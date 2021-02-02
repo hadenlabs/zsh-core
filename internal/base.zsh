85d9beb233f4ed605e17b2fd6e95bb834c4a99a9
@slovacus <slovacus@gmail.com>
2021-02-02 16:25:45 -0500

    chore: implement internal tools for install (#5)

--- internal/base.zsh
+++ internal/base.zsh
@@ -23,3 +23,63 @@ function core::internal::curl::install {
     brew install curl
     message_success "Installed curl for ${CORE_PACKAGE_NAME}"
 }
+
+function core::internal::less::install {
+    message_info "Installing less for ${CORE_PACKAGE_NAME}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${CORE_MESSAGE_BREW}"
+        return
+    fi
+    brew install less
+    message_success "Installed less for ${CORE_PACKAGE_NAME}"
+
+}
+
+function core::internal::most::install {
+    message_info "Installing most for ${CORE_PACKAGE_NAME}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${CORE_MESSAGE_BREW}"
+        return
+    fi
+    brew install most
+    message_success "Installed most for ${CORE_PACKAGE_NAME}"
+
+}
+
+function core::internal::rsync::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${CORE_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Installing rsync for ${CORE_PACKAGE_NAME}"
+    brew install rsync
+    message_success "Installed rsync ${CORE_PACKAGE_NAME}"
+}
+
+function core::internal::fzf::install {
+    # shellcheck disable=SC2154
+    message_info "Installing fzf for ${CORE_PACKAGE_NAME}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${CORE_MESSAGE_BREW}"
+        return
+    fi
+    brew install fzf
+    message_success "Installed fzf for ${CORE_PACKAGE_NAME}"
+}
+
+function core::internal::yarn::install {
+    message_info "Installing yarn for ${CORE_PACKAGE_NAME}"
+    curl -o- -L https://yarnpkg.com/install.sh | bash
+    message_success "Installed yarn ${CORE_PACKAGE_NAME}"
+}
+
+function core::internal::node::install {
+    if ! type nvm > /dev/null; then
+        message_warning "${CORE_MESSAGE_NVM}"
+        return
+    fi
+    message_info "Installing version node ${CORE_PACKAGE_NAME}"
+    nvm install --lts
+    nvm use --lts --default
+    message_success "Installed version node ${CORE_PACKAGE_NAME}"
+}
