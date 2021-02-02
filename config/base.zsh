7969ed3c7a4176fa10b618c8f4e979b4a4a1a732
@slovacus <slovacus@gmail.com>
2021-02-02 16:24:44 -0500

    chore: implement configuration for messages not found (#5)

--- config/base.zsh
+++ config/base.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export CORE_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export CORE_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
+export CORE_MESSAGE_YAY="Please install Go or use antibody bundle hadenlabs/zsh-goenv branch:develop"
+export CORE_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm branch:develop"
+export CORE_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm branch:develop"
