b679044bc0fa117e4f1d265a415d6e38137ffffd
Luis Mayta <luis@hadenlabs.com>
2021-02-02 16:04:46 -0500

    chore: implement load zsh command (#3)

---
+++ zsh-core.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+#
+# Defines install core for osx or linux.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
+
+# shellcheck disable=SC2034  # Unused variables left for readability
+ZSH_CORE_PATH=$(dirname "${0}")
+
+# shellcheck source=/dev/null
+source "${ZSH_CORE_PATH}"/config/main.zsh
+
+# shellcheck source=/dev/null
+source "${ZSH_CORE_PATH}"/internal/main.zsh
+
+# shellcheck source=/dev/null
+source "${ZSH_CORE_PATH}"/pkg/main.zsh
