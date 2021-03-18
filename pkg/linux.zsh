05cd8db163171195ac6c1da96203be1b84f6ded2
@slovacus <slovacus@gmail.com>
2021-03-18 11:30:09 -0500

    chore: implement functions helpers (#14)

--- pkg/linux.zsh
+++ pkg/linux.zsh
@@ -1,2 +1,28 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+if ! core::exists xclip; then core::install xclip; fi
+
+function open {
+    if [ -e /usr/bin/xdg-open ]; then
+        xdg-open "${1}"
+    fi
+}
+
+function pbcopy {
+    if type xclip > /dev/null; then
+        xclip -selection clipboard
+    fi
+    if type xsel > /dev/null; then
+        xsel --clipboard --input
+    fi
+}
+
+function pbpaste {
+    if type xclip > /dev/null; then
+        xclip -selection clipboard -o
+    fi
+    if type xsel > /dev/null; then
+        xsel --clipboard --output
+    fi
+}
