ae52cc58bcadf98039c400e4cf4d76022396a0c1
Luis Mayta <luis@hadenlabs.com>
2021-03-21 14:02:14 -0500

    chore: implement alias and helper (#16)

--- pkg/alias.zsh
+++ pkg/alias.zsh
@@ -1,2 +1,18 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+if ! core::exists exa; then core::install exa; fi
+
+function exa::load {
+    alias l='exa --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+    alias ls='exa --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+    alias ll='exa -l --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+    alias lll='exa -l --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale | less'
+    alias lla='exa -la --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+    alias llt='exa -T --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+    alias llfu='exa -bghHliS --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+}
+
+if core::exists exa; then
+    exa::load
+fi
---
+++ pkg/docker.zsh
@@ -0,0 +1,50 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function awscli {
+    docker run --rm -it \
+           -v "$(pwd):/home/nikovirtala" \
+           -v "${HOME}/.aws:/home/nikovirtala/.aws" \
+           -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+           -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+           -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
+           nikovirtala/awscli:latest "$@"
+}
+
+function aws-shell {
+    docker run --rm -it \
+           -v "$(pwd):/home/nikovirtala" \
+           -v "${HOME}/.aws:/home/nikovirtala/.aws" \
+           -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+           -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+           -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
+           nikovirtala/aws-shell:latest "$@"
+}
+
+# show nyancat in terminal
+function nyancat {
+    docker run -it --rm supertest2014/nyan;
+}
+
+function ytd-mp3 {
+    docker run --rm -v "${PWD}":/data hadenlabs/youtube-dl --extract-audio --audio-format mp3 "$@"
+}
+
+function ytdl {
+    docker run --rm -v "${PWD}":/data hadenlabs/youtube-dl "$@"
+}
+
+function youtube-dl {
+    docker run --rm -v "${PWD}":/data hadenlabs/youtube-dl "$@"
+}
+
+function pandoc {
+    docker run --rm -v "${PWD}":/source hadenlabs/pandoc "$@"
+}
+
+function komiser {
+    docker run --rm -d -p 3000:3000 \
+           -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+           -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+           -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" --name komiser mlabouardy/komiser
+}
