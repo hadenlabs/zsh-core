5c9e367916bf9c0adb893df50bf3a566c64f5b75
Luis Mayta <luis@hadenlabs.com>
2021-04-02 18:46:27 -0500

    chore: implement list package in cask (#22)

--- config/base.zsh
+++ config/base.zsh
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 export CORE_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
-export CORE_MESSAGE_YAY="Please install Go or use antibody bundle hadenlabs/zsh-goenv branch:develop"
+export CORE_MESSAGE_YAY="Please install Yay or use antibody bundle hadenlabs/zsh-yay branch:develop"
 export CORE_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm branch:develop"
 export CORE_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm branch:develop"
