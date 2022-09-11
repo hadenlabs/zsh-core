e3160c00e33ee8ee9052fa924cafa826514e4bcd
Luis Mayta <luis@hadenlabs.com>
2022-09-11 16:24:28 -0300

    style: format app zsh to branch main (#34)

--- config/base.zsh
+++ config/base.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export CORE_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
-export CORE_MESSAGE_YAY="Please install Yay or use antibody bundle hadenlabs/zsh-yay branch:develop"
-export CORE_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm branch:develop"
-export CORE_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm branch:develop"
+export CORE_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
+export CORE_MESSAGE_YAY="Please install Yay or use antibody bundle hadenlabs/zsh-yay"
+export CORE_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm"
+export CORE_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm"
