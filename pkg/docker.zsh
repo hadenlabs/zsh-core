ffd1a37da65430bafa76abd74c67bc0b2bcdb46f
Luis Mayta <luis@hadenlabs.com>
2023-01-13 20:48:59 -0500

    fix: execute komiser using docker tag 2.4.0 (#36)
    * refactor: sync configuration project (#36)
    
    * refactor: task actions project (#36)
    
    * feat: change workflow gitflow to github-flow (#36)
    
    * fix: configuration sonar properties (#36)
    
    * fix: implement load komiser using docker (#36)
    
    * build: generate files (#36)

--- pkg/docker.zsh
+++ pkg/docker.zsh
@@ -46,5 +46,5 @@ function komiser {
     docker run --rm -d -p 3000:3000 \
            -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
            -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-           -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" --name komiser mlabouardy/komiser
+           -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" --name komiser mlabouardy/komiser:2.4.0
 }
