201b45620a0436af43e5f0dce8dd6a9e84e4ac8f
Luis Mayta <luis@hadenlabs.com>
2024-06-02 19:23:07 -0300

    refactor: split function to osx and android (#42)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -27,38 +27,55 @@ function core::internal::core::install {
 
 }
 
+function core::internal::android::install {
+
+  message_info "Installing android"
+
+  touch "${ANDROID_FILE_REPOSITORIES}"
+  mkdir -p "${ANDROID_HOME}"
+
+  [ "$(! core::exists java)" ] && brew install openjdk@11 openjdk@17
+  [ "$(! core::exists sdkmanager)" ] && brew install --cask android-commandlinetools
+
+  sdkmanager --update --sdk_root="${ANDROID_HOME}"
+  sdkmanager --install "cmdline-tools;latest" --sdk_root="${ANDROID_HOME}"
+  yes | sdkmanager "platforms;android-${ANDROID_PLATFORM_VERSION}" --sdk_root="${ANDROID_HOME}"
+  yes | sdkmanager "platform-tools" --sdk_root="${ANDROID_HOME}"
+  yes | sdkmanager "platform-tools" "build-tools;${ANDROID_SDK_VERSION}" --sdk_root="${ANDROID_HOME}"
+  yes | sdkmanager "extras;google;m2repository" --sdk_root="${ANDROID_HOME}"
+  yes | sdkmanager "extras;android;m2repository" --sdk_root="${ANDROID_HOME}"
+
+  message_success "android Installed"
+}
+
+function core::internal::osx::dependences::install {
+  message_info "Installing Dependences for osx"
+  if ! core::exists gem; then
+    message_warning "${CORE_MESSAGE_RVM}"
+    return
+  fi
+
+  gem install cocoapods
+
+  brew install --HEAD usbmuxd
+  brew link usbmuxd
+  brew install --HEAD libimobiledevice
+  brew install ideviceinstaller cocoapods ios-deploy
+  pod setup
+
+  message_success "osx Dependences Installed"
+}
+
 function core::internal::multiplatform::install {
-    if ! core::exists brew; then
-        message_info "${CORE_MESSAGE_BREW}"
-        return
-    fi
+  if ! core::exists brew; then
+    message_warning "${CORE_MESSAGE_BREW}"
+    return
+  fi
 
-    message_info "Installing Dependences for multiplatform"
+  message_info "Installing Dependences for multiplatform"
 
-    gem install cocoapods
+  core::internal::osx::dependences::install
+  core::internal::android::install
 
-    brew install --HEAD usbmuxd
-    brew link usbmuxd
-    brew install --HEAD libimobiledevice
-    brew install ideviceinstaller cocoapods ios-deploy
-    pod setup
-
-    touch "${ANDROID_FILE_REPOSITORIES}"
-    mkdir -p "${ANDROID_HOME}"
-
-    [ "$(! core::exists java)" ] && brew install openjdk@11 openjdk@17
-    [ "$(! core::exists sdkmanager)" ] && brew install --cask android-commandlinetools
-
-    sdkmanager --update --sdk_root="${ANDROID_HOME}"
-    sdkmanager --install "cmdline-tools;latest" --sdk_root="${ANDROID_HOME}"
-    yes | sdkmanager "platforms;android-${ANDROID_PLATFORM_VERSION}" --sdk_root="${ANDROID_HOME}"
-    yes | sdkmanager "platform-tools" --sdk_root="${ANDROID_HOME}"
-    yes | sdkmanager "platform-tools" "build-tools;${ANDROID_SDK_VERSION}" --sdk_root="${ANDROID_HOME}"
-    yes | sdkmanager "extras;google;m2repository" --sdk_root="${ANDROID_HOME}"
-    yes | sdkmanager "extras;android;m2repository" --sdk_root="${ANDROID_HOME}"
-
-    flutter config --android-sdk "${ANDROID_HOME}"
-
-    yes | flutter doctor --android-licenses --sdk_root="${ANDROID_HOME}"
-    message_success "Multiplatform Dependences Installed"
+  message_success "Multiplatform Dependences Installed"
 }
