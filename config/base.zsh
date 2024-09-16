d804d0b2aed8353f95893fd7fc3c28edb2af0aae
Luis Mayta <luis@hadenlabs.com>
2024-09-16 14:00:19 -0300

    fix(core): change path android home

--- config/base.zsh
+++ config/base.zsh
@@ -6,7 +6,7 @@ export CORE_MESSAGE_YAY="Please install Yay or use antibody bundle hadenlabs/zsh
 export CORE_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm"
 export CORE_MESSAGE_NVM="Please install nvm or use antibody bundle hadenlabs/zsh-nvm"
 
-export ANDROID_HOME="${HOME}/android-sdk"
+export ANDROID_HOME="${HOME}/Library/Android/sdk"
 export ANDROID_PLATFORM_VERSION="35"
 export ANDROID_SDK_VERSION="35.0.1"
 export ANDROID_FILE_REPOSITORIES="${HOME}/.android/repositories.cfg"
