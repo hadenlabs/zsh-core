af3960fbc1c5e216c4fb7cc0a830cfb9c3b7f166
Luis <slovacus@gmail.com>
2024-11-10 20:18:55 -0300

    chore(core): change exa to eza (#44)

--- pkg/alias.zsh
+++ pkg/alias.zsh
@@ -1,22 +1,23 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-if ! core::exists exa; then core::install exa; fi
-
-function exa::load {
-    alias l='exa --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
-    alias ls='exa --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
-    alias ll='exa -l --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
-    alias lll='exa -l --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale | less'
-    alias lla='exa -la --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
-    alias llt='exa -T --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
-    alias llfu='exa -bghHliS --all --grid --git --sort=ext --color=always --group-directories-first --icons --color-scale'
+function core::cargo::install {
+  core::internal::cargo::install "${@}"
 }
 
-if core::exists exa; then
-    exa::load
-fi
+function eza::load {
+
+  alias ls='eza --icons=auto'
+  alias ll='eza --long --header --git --group --icons=auto'
+  alias lsa='eza --long --header --git --group --all --icons=auto'
+}
 
 function core::multiplatform::install {
   core::internal::multiplatform::install
 }
+
+if ! core::exists eza; then core::cargo::install eza; fi
+
+if core::exists eza; then
+    eza::load
+fi
